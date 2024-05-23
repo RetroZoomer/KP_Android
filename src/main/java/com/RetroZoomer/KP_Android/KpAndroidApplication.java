bc1bcd8913f8f491322c142ef5c79912d0eff518
RetroZoomer <bayan-herov@mail.ru>
2024-05-23 17:08:06 +0300

    .

--- src/main/java/com/RetroZoomer/KP_Android/KpAndroidApplication.java
+++ src/main/java/com/RetroZoomer/KP_Android/KpAndroidApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KpAndroidApplication {
-//kdfdsjfslkdjfdsfsdfdf
 	public static void main(String[] args) {
 		SpringApplication.run(KpAndroidApplication.class, args);
 	}
